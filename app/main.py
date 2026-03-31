36d2c604788d610096255e17b2691c74f0dd3c66
Ihor Herasymenko <igor.harley.86@gmail.com>
2026-03-31 13:41:28 +0300

    'Solution#2'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def copy_file(command: str) -> None:
     try:
         parts = command.strip().split()
         if len(parts) != 3 or parts[0] != "cp":
-            raise ValueError("Invalid command format!")
+            raise ValueError
 
         _, original_file, new_file = parts
         if original_file == new_file:
@@ -14,10 +14,6 @@ def copy_file(command: str) -> None:
             file_out.write(content)
 
     except FileNotFoundError:
-        print(f"Error: File {original_file} not found.")
-    except PermissionError:
-        print("Error: Permission denied.")
-    except ValueError as err:
-        print(f"Error: {err}")
-    except Exception as err:
-        print(f"Unexpected error: {err}")
+        pass
+    except ValueError:
+        pass
