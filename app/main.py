6d01594f7030457d319fc4f7d85880e802a362cc
Ihor Herasymenko <igor.harley.86@gmail.com>
2026-03-31 13:46:48 +0300

    'Solution#3'

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
 def copy_file(command: str) -> None:
+
+    parts = command.strip().split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    _, original_file, new_file = parts
+    if original_file == new_file:
+        return
+
     try:
-        parts = command.strip().split()
-        if len(parts) != 3 or parts[0] != "cp":
-            raise ValueError
-
-        _, original_file, new_file = parts
-        if original_file == new_file:
-            return
-
         with (open(original_file, "r") as file_in,
               open(new_file, "w") as file_out):
             content = file_in.read()
             file_out.write(content)
 
     except FileNotFoundError:
-        pass
-    except ValueError:
-        pass
+        return
